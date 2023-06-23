305aec2a51983cabeafac216839c4b2d63e478da
chrisdotgrubb <developer.grubb@gmail.com>
2023-06-23 08:20:15 -0400

    Add customer show page

--- controllers/customers.js
+++ controllers/customers.js
@@ -2,6 +2,7 @@ const Customer = require('../models/customer');
 
 module.exports = {
 	index,
+	show
 };
 
 
@@ -12,6 +13,24 @@ async function index(req, res) {
 			customers,
 		};
 		res.render('customers/index', context);
+	} catch (err) {
+		const context = {
+			error: err,
+			message: err.message,
+		};
+		res.render('error', context);
+	};
+}
+
+async function show(req, res) {
+	const id = req.params.id;
+	try {
+		const customer = await Customer.findById(id);
+		const context = {
+			customer
+		};
+		res.render('customer/show', context);
+
 	} catch (err) {
 		const context = {
 			error: err,
--- routes/customers.js
+++ routes/customers.js
@@ -5,5 +5,7 @@ const customerController = require('../controllers/customers');
 
 //		
 router.get('/', customerController.index);
+router.get('/:id', customerController.show);
+
 
 module.exports = router;
