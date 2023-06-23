439e4ba4691c9b25a7e91d7e86cc31aab4ca4562
chrisdotgrubb <developer.grubb@gmail.com>
2023-06-23 10:10:07 -0400

    Add embedded car model

--- models/customer.js
+++ models/customer.js
@@ -2,6 +2,44 @@ const mongoose = require('mongoose');
 
 const Schema = mongoose.Schema;
 
+const carSchema = new Schema({
+	year: {
+		type: Number,
+		required: true,
+	},
+	make: {
+		type: String,
+		required: true,
+	},
+	model: {
+		type: String,
+		required: true,
+	},
+	trimLevel: {
+		type: String,
+		required: true,
+	},
+	licensePlate: {
+		type: String,
+	},
+	mileage: {
+		type: Number,
+		required: true,
+	},
+	color: {
+		type: String,
+	},
+	engine: {
+		type: String,
+		required: true,
+	},
+	tireSize: {
+		type: String,
+	},
+}, {timestamps: true}
+);
+
+
 const customerSchema = new Schema({
 	first: {
 		type: String,
@@ -23,6 +61,7 @@ const customerSchema = new Schema({
 		type: String,
 		required: true,
 	},
+	cars: [carSchema],
 }, {timestamps: true});
 
 module.exports = mongoose.model('Customer', customerSchema);
