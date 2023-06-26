df7cce6b3153ee245d2196b0f9c2a3b5cb8a0189
chrisdotgrubb <developer.grubb@gmail.com>
2023-06-26 09:42:54 -0400

    Add services show page

--- controllers/services.js
+++ controllers/services.js
@@ -2,7 +2,7 @@ const Customer = require('../models/customer');
 
 module.exports = {
 	index,
-	// show,
+	show,
 	// new: newCustomer,
 	// create,
 	// delete: deleteCustomer,
@@ -21,6 +21,30 @@ async function index(req, res) {
 			car,
 		};
 		res.render('services/index', context);
+	} catch (err) {
+		const context = {
+			error: err,
+			message: err.message,
+		};
+		res.render('error', context);
+	};
+}
+
+async function show(req, res) {
+	const customerId = req.params.customerId;
+	const carId = req.params.carId;
+	const serviceId = req.params.serviceId;
+	try {
+		const customer = await Customer.findById(customerId);
+		const car = customer.cars.id(carId);
+		const service = car.services.id(serviceId);
+		const context = {
+			customer,
+			car,
+			service,
+		};
+		res.render('services/show', context);
+
 	} catch (err) {
 		const context = {
 			error: err,
