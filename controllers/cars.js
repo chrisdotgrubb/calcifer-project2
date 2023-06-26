c1fc925155bc5808cf3cc8db0db004ca6d250de1
chrisdotgrubb <developer.grubb@gmail.com>
2023-06-26 17:33:56 -0400

    Add index to get embedded errors

--- controllers/cars.js
+++ controllers/cars.js
@@ -55,6 +55,10 @@ async function newCar(req, res) {
 		const customer = await Customer.findById(customerId);
 		const context = {
 			customer,
+			err: {
+				errors: '',
+			},
+			index: 0,
 		};
 		res.render('cars/new', context);
 	} catch (err) {
@@ -64,22 +68,38 @@ async function newCar(req, res) {
 		};
 		res.render('error', context);
 	};
-
 };
 
 async function create(req, res) {
 	const customerId = req.params.customerId;
+	let customer;
 	try {
-		const customer = await Customer.findById(customerId);
+		customer = await Customer.findById(customerId);
 		customer.cars.push(req.body);
 		await customer.save();
 		res.redirect(`/customers/${customerId}/cars`);
 	} catch (err) {
-		const context = {
-			error: err,
-			message: err.message,
-		};
-		res.render('error', context);
+		if (customer) {
+			console.log(err.errors);
+			let index = 0;
+			if (err._message === 'Customer validation failed') {
+				// get the embedded model's index, since error comes back like: err.errors['cars.0.make']
+				index = Object.keys(err.errors)[0].match(/cars\.(\d*)\..*/)[1];
+			}
+			const context = {
+				customer,
+				err,
+				index,
+				message: err.message,
+			};
+			res.render('cars/new', context);
+		} else {
+			const context = {
+				err,
+				message: err.message,
+			};
+			res.render('error', context);
+		}
 	};
 }
 
@@ -110,6 +130,10 @@ async function edit(req, res) {
 		const context = {
 			customer,
 			car,
+			err: {
+				errors: '',
+			},
+			index: 0,
 		};
 		res.render('cars/edit', context);
 	} catch (err) {
@@ -133,11 +157,16 @@ async function update(req, res) {
 		await customer.save();
 		res.redirect(`/customers/${customerId}/cars/${carId}`);
 	} catch (err) {
-		console.log(err);
+		let index = 0;
+		if (err._message === 'Customer validation failed') {
+			// get the embedded model's index, since error comes back like: err.errors['cars.0.make']
+			index = Object.keys(err.errors)[0].match(/cars\.(\d*)\..*/)[1];
+		};
 		const context = {
 			customer,
 			car: body,
-			error: err,
+			err,
+			index,
 		};
 		// add carId so form action has access to it after validation error
 		context.car.id = carId;
